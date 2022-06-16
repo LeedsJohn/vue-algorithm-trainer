fd2b34d899a38b1934f3c191f49eb0879c576ede
John Leeds <leedsjohn6@gmail.com>
2022-06-16 14:10:46 -0500

    init

---
+++ src/assets/scrambles/Groupings.js
@@ -0,0 +1,32 @@
+const groupings = {
+  WV: {
+    "3 corners": ["WV 1"],
+    "2 corners": ["WV 2", "WV 3", "WV 4", "WV 5", "WV 6", "WV 7"],
+    "1 corner": [
+      "WV 8",
+      "WV 9",
+      "WV 10",
+      "WV 11",
+      "WV 12",
+      "WV 13",
+      "WV 14",
+      "WV 15",
+      "WV 16",
+      "WV 17",
+      "WV 18",
+      "WV 19",
+    ],
+    "0 corners": [
+      "WV 20",
+      "WV 21",
+      "WV 22",
+      "WV 23",
+      "WV 24",
+      "WV 25",
+      "WV 26",
+      "WV 27",
+    ],
+  },
+};
+
+exports.groupings = groupings;
