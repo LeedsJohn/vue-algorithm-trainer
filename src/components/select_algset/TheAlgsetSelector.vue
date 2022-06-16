bea5ee5404809a902aa5ad1eb00fd0d658ac108a
John Leeds <leedsjohn6@gmail.com>
2022-06-16 11:59:14 -0500

    init

---
+++ src/components/select_algset/TheAlgsetSelector.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul v-for="set in algSets" :key="set.file">
+    <li>
+      <base-button @click="chooseSet(set.name)">{{ set.name }}</base-button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      algSets: [
+        {
+          name: "Winter Variation",
+          file: "WV.js",
+        },
+      ],
+    };
+  },
+};
+</script>
