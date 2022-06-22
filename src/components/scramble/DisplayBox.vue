d6a8b065e2adbc00411a72139d40410490695b1c
John Leeds <leedsjohn6@gmail.com>
2022-06-22 12:44:50 -0500

    init

---
+++ src/components/scramble/DisplayBox.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="box-container">
+    <ul>
+      <li v-for="i in numAlgs" :key="i">
+        {{ this.algNames[i-1] }}<span v-if="i !== algNames.length">, </span>
+      </li>
+      <li v-if="algNames.length > 10">...</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["algNames"],
+  computed: {
+    numAlgs() {
+      return Math.min(this.algNames.length, 10);
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .box-container {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    border: solid #000;
+    border-radius: 10%;
+    display: inline-block;
+  }
+  ul {
+    display: inline;
+  }
+  li {
+    list-style: none;
+    display: inline;
+    font-weight: 600;
+    font-size: 1.1vw;
+    float: left;
+    margin: 0 3px;
+  }
+
+  span {
+    margin-right: 2px;
+  }
+</style>
---
+++ src/components/scramble/DisplayBoxes.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul>
+    <li v-for="i in 5" :key="i">
+      <display-box :algNames="this.boxes[i-1]"></display-box>
+    </li>
+  </ul>
+</template>
+
+<script>
+import DisplayBox from "./DisplayBox.vue";
+export default {
+  components: {
+    DisplayBox,
+  },
+  props: ["algTrainer"],
+  computed: {
+    boxes() {
+      const arr = [];
+      arr.push(this.algTrainer.getAllAlgs(true, 0));
+      arr.push(this.algTrainer.getAllAlgs(true, 1));
+      arr.push(this.algTrainer.getAllAlgs(true, 2, 3));
+      arr.push(this.algTrainer.getAllAlgs(true, 4));
+      arr.push(this.algTrainer.getAllAlgs(true, 5));
+      console.log(arr);
+      return arr;
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  position: absolute;
+  bottom: 10%;
+  display: grid;
+  grid-template-columns: 10vw 10vw 10vw 10vw 10vw;
+  grid-template-rows: 10vw;
+  column-gap: 2%;
+}
+
+li {
+  list-style: none;
+  display: inline;
+}
+</style>
