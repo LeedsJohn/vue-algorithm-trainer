b0a4e23b773e6492dae9c66414ae3d8e936c570b
John Leeds <leedsjohn6@gmail.com>
2022-06-28 11:01:55 -0500

    init

---
+++ src/components/scramble/AlgSolutions.vue
@@ -0,0 +1,27 @@
+<template>
+  <base-foreground>
+    <ul>
+      <li v-for="solution in solutions" :key="solution">
+        {{ solution }}
+      </li>
+    </ul>
+  </base-foreground>
+</template>
+
+<script>
+import BaseForeground from "../UI/BaseForeground.vue";
+
+export default {
+  components: {
+    BaseForeground,
+  },
+  props: ["solutions"],
+}
+</script>
+
+<style scoped>
+ul {
+  height: 60vh;
+  width: 60vw;
+}
+</style>
