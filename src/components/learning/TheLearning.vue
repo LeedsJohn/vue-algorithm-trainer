46ea9a7b52f818a70c4ca39dc923dd3727d64a42
John Leeds <leedsjohn6@gmail.com>
2022-07-05 17:41:59 -0500

    Init

---
+++ src/components/learning/TheLearning.vue
@@ -0,0 +1,26 @@
+<template>
+  <the-scramble v-show="active === 'scramble'"></the-scramble>
+  <the-alg-selector v-if="active === 'alg-selector'" @selectAlgs="selectAlgs"></the-alg-selector>
+</template>
+
+<script>
+import TheAlgSelector from "./select_algs/TheAlgSelector.vue";
+import TheScramble from "./TheScramble.vue";
+
+export default {
+  components: {
+    TheAlgSelector,
+    TheScramble,
+  },
+  data() {
+    return {
+      active: "scramble",
+    };
+  },
+  methods: {
+    selectAlgs() {
+      this.active = "alg-selector";
+    }
+  }
+};
+</script>
