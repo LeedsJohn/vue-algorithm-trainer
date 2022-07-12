62e649ba9a5b6991d48fae07f9213291ef17f41c
John Leeds <leedsjohn6@gmail.com>
2022-07-12 12:56:31 -0500

    Basic styling

--- src/components/learning/TheSuggestions.vue
+++ src/components/learning/TheSuggestions.vue
@@ -1,18 +1,21 @@
 <template>
   <base-foreground type="screen" @close="close">
-    <p v-for="alg in suggestions" :key="alg">
-      {{ alg }}
-    </p>
+  <h1>Suggested Algorithms</h1>
+    <div>
+      <p class="algName" v-for="alg in suggestions" :key="alg">
+        {{ alg }}
+      </p>
+    </div>
   </base-foreground>
 </template>
 
 <script>
-export default { 
+export default {
   props: ["algSet", "algNames"],
   emits: ["close"],
   mounted() {
     for (const alg of this.algNames) {
-      if (localStorage[`${this.algSet}${alg}Wrong`] === '1') {
+      if (localStorage[`${this.algSet}${alg}Wrong`] === "1") {
         this.suggestions.push(alg);
       }
     }
@@ -20,12 +23,28 @@ export default {
   data() {
     return {
       suggestions: [],
-    }
+    };
   },
   methods: {
     close() {
       this.$emit("close");
-    }
+    },
   },
-}
+};
 </script>
+
+<style scoped>
+div {
+  padding: 2%;
+}
+
+h1 {
+  font-size: 1.5rem;
+  margin-bottom: 0;
+}
+
+.algName {
+  text-align: left;
+  margin: 10px 0;
+}
+</style>
